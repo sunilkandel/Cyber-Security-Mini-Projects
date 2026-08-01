3912845de3e707879df09d188993fdaba71f7643
sunilkandel <sunilkandel726@gmail.com>
2026-08-01 23:14:13 +0545

    Wrote the File encryption and decryption python file using python package called Cyptography and it's module fernet (contains Fernet class which used for encryption and decryption).

--- Src/File_encrypt_AND_decrypt/main.py
+++ Src/File_encrypt_AND_decrypt/main.py
@@ -1,42 +1,80 @@
 from cryptography.fernet import Fernet
 
-def generate_key(key_path="secret.key"):
-    key = Fernet.generate_key()
-    with open(key_path, "wb") as f:
-        f.write(key)
-    print(f"Key saved to {key_path}")
 
-def load_key(key_path="secret.key"):
-    with open(key_path, "rb") as f:
+def generate_key(keypath="secret.key"):
+    key = Fernet.generate_key()
+    with open (keypath, "wb") as f:
+        f.write(key)
+
+    print(f"Key Saved to {keypath}")
+
+def load_key(keypath = "secret.key"):
+    with open (keypath, 'rb') as f:
         return f.read()
 
-def encrypt_file(filepath, key):
-    # TODO:
-    # 1. Read the file in binary mode
-    # 2. Create a Fernet object with the key
-    # 3. Encrypt the data
-    # 4. Write it to filepath + ".enc"
-    pass
 
-def decrypt_file(filepath, key):
-    # TODO: reverse of encrypt_file
-    # careful: what should the output filename be?
-    pass
+
+def encrypt_file(file, key):
+    # TODO:
+    # 1. Open and read the file in binary mode
+    with open (file, 'rb') as f:
+        original_data = f.read()
+
+    # 2. Create a Fernet object using the key
+    fernet = Fernet(key)
+
+    # 3. Call the method that encrypts data
+    encrypted_data = fernet.encrypt(original_data)
+    
+    # 4. Write the result to a new file (decide the naming convention yourself)
+    new_file_name = file.replace(".txt", "_encrypted.enc")
+    with open(new_file_name, "wb") as encrypted_file:
+        encrypted_file.write(encrypted_data)
+
+
+def decrypt_file(file, key):
+    # TODO: same shape, reversed
+    fernet = Fernet(key)
+
+    
+    #Read the encrypted file in binary
+    with open(file, "rb") as encrypted_file:
+        encrypted_data = encrypted_file.read()
+
+
+    #Decrypt the encrypted data
+    decrypted_data = fernet.decrypt(encrypted_data)
+
+    # Remove .enc from the original file and add '_decrypted.txt' as an extention
+    original_file_name = file.replace("_encrypted.enc", "_decrypted.dnc")
+
+    # Save decrypted data to the file
+    with open(original_file_name, "wb") as decrypted_file:
+        decrypted_file.write(decrypted_data)
 
 
 if __name__ == "__main__":
     import sys
+
     if len(sys.argv) < 2:
-        print("Usage: python file_crypto.py [genkey|encrypt|decrypt] [filename]")
+        print("Usage: python main.py [ genkey 'G'|encrypt 'E' |decrypt 'D' ] [Filename]")
         sys.exit(1)
 
     action = sys.argv[1]
+    #file = sys.argv[2]
 
-    if action == "genkey":
+    if (action == "genkey") | (action == "G"):
         generate_key()
-    elif action == "encrypt":
+
+    elif (action == "encrypt") | (action == "E"):
+        file = sys.argv[2]
         key = load_key()
-        encrypt_file(sys.argv[2], key)
-    elif action == "decrypt":
+        encrypt_file(file,key)
+
+    if (action == "genkey") | (action == "D"):
+        file = sys.argv[2]
         key = load_key()
-        decrypt_file(sys.argv[2], key)
+        decrypt_file(file, key)
+
+
+    
