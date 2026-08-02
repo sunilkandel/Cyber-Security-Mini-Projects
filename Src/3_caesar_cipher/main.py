b0335fb2945c9cb8480acededf14c50885937bda
sunilkandel <sunilkandel726@gmail.com>
2026-08-02 22:02:47 +0545

    Getting started with 3rd project 'Caesar Cipher' using python only.

---
+++ Src/3_caesar_cipher/main.py
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    msg = input("Enter your message: ")
+    shift = input("Enter shift value : ")
+    mode = input ("Encrypt or Decrypt? (e/d): ")
+
+    if  mode.lower() == "e":
+        print("You made it.")
+
+    else:
+        print("You still made it?")
